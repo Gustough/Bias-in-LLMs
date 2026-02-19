f54b2425011bb263467d50197a5601309ab21272
Gustough <135703137+Gustough@users.noreply.github.com>
2026-02-19 18:39:16 +0100

    i tried

--- webscraper.py
+++ webscraper.py
@@ -4,21 +4,23 @@ import sys
 import re
 import os.path
 
+def get_url(page_number):
+    endpoint = f"https://ec.europa.eu/commission/presscorner/api/latestnews?language=en&datefrom=01011978&dateto=31122026&pagesize=20&pagenumber={page_number}"
+    r = requests.get(endpoint)
+    data = r.json()
+    url = data.get("docuLanguageListResources", {})[page_number].get("refCode")
+    return url
+
 def scrape_article(url, language):    
-    ip = url.split('/')[-1].split("_")[1:]
-    base = 'https://ec.europa.eu/commission/presscorner/api/documents?reference=IP/'
-    
-    api_url = base + ip[0] + "/" + ip[1] + f'&language={language}'
+    base = 'https://ec.europa.eu/commission/presscorner/api/documents?reference='
+    api_url = base + url + f'&language={language}'
     r = requests.get(api_url)
     data = r.json()
     date = data.get("eventDate")
     topics = [item.get("code") for item in data.get("policiesResource", [])]
     html_content = data.get("docuLanguageResource", {}).get("htmlContent", "")
-
     soup = bs4.BeautifulSoup(html_content, 'html.parser')
-    
     paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
-    
     return date, topics, paragraphs
 
 
@@ -51,15 +53,16 @@ if __name__ == '__main__':
         "sv",  # svenska
     ]
 
-    for url in sys.argv[1:]:
+    for number in range(2, 50):
         for language in language_codes:
+            url = get_url(number)
             date, topics, paragraphs = scrape_article(url, language)
-            if paragraphs:
-                for number in range(1, 1000000):
-                    filename = f'{language}-{date}-{number}.txt'
-                    if not os.path.exists(os.path.join(data_path, filename)):
-                        break
-                with open(os.path.join(data_path, filename), 'w', encoding="utf-8") as f:
-                    print(topics, file=f)
-                    for paragraph in paragraphs:
-                        print(paragraph, file=f)
+            filename = f'{language}-{date}-{number}.txt'
+            
+            # if not os.path.exists(os.path.join(data_path, filename)):
+            #     break
+            
+            with open(os.path.join(data_path, filename), 'w', encoding="utf-8") as f:
+                print(topics, file=f)
+                for paragraph in paragraphs:
+                    print(paragraph, file=f)
