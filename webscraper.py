435f30081b76846243f78a304772169fcc3380c9
Gustough <135703137+Gustough@users.noreply.github.com>
2026-02-20 16:34:17 +0100

    Extended the webscraper. Collects articles for a specified set of languages and creates list of topic-tags.

--- webscraper.py
+++ webscraper.py
@@ -4,12 +4,22 @@ import sys
 import re
 import os.path
 
-def get_url(page_number):
+def get_url(page_number, id):
     endpoint = f"https://ec.europa.eu/commission/presscorner/api/latestnews?language=en&datefrom=01011978&dateto=31122026&pagesize=20&pagenumber={page_number}"
     r = requests.get(endpoint)
     data = r.json()
-    url = data.get("docuLanguageListResources", {})[page_number].get("refCode")
-    return url
+    number_of_articles = len(data.get("docuLanguageListResources"))
+    url = data.get("docuLanguageListResources", {})[id].get("refCode")
+    return url, number_of_articles
+
+def get_languages(url):
+    base = 'https://ec.europa.eu/commission/presscorner/api/documents?reference='
+    api_url = base + url + "&language=en"
+    r = requests.get(api_url)
+    data = r.json()
+    LANGUAGES = data.get("languages", {})
+    languages = list(map(str.lower, LANGUAGES))
+    return languages
 
 def scrape_article(url, language):    
     base = 'https://ec.europa.eu/commission/presscorner/api/documents?reference='
@@ -18,15 +28,16 @@ def scrape_article(url, language):
     data = r.json()
     date = data.get("eventDate")
     topics = [item.get("code") for item in data.get("policiesResource", [])]
+    topic_descriptions = [item.get("description") for item in data.get("policiesResource", [])]
     html_content = data.get("docuLanguageResource", {}).get("htmlContent", "")
     soup = bs4.BeautifulSoup(html_content, 'html.parser')
     paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
-    return date, topics, paragraphs
+    return date, topics, topic_descriptions, paragraphs
 
 
 if __name__ == '__main__':
     data_path = "Data"
-    language_codes = [
+    language_codes = [ # List of languages to scrape
         "bg",  # български
         "cs",  # čeština
         "da",  # dansk
@@ -52,17 +63,34 @@ if __name__ == '__main__':
         "sl",  # slovenščina
         "sv",  # svenska
     ]
-
-    for number in range(2, 50):
-        for language in language_codes:
-            url = get_url(number)
-            date, topics, paragraphs = scrape_article(url, language)
-            filename = f'{language}-{date}-{number}.txt'
+    topic_list = set()
+    
+    for number in range(1, 5): #Number of articles to scrape
+        counter = 1
+        thingy = 0
+        _, article_ids = get_url(number, thingy)
+        
+        for article_id in range(article_ids):
+            url, _ = get_url(number, article_id)
+            languages = get_languages(url)
+            languages.append("en")
             
-            # if not os.path.exists(os.path.join(data_path, filename)):
-            #     break
-            
-            with open(os.path.join(data_path, filename), 'w', encoding="utf-8") as f:
-                print(topics, file=f)
-                for paragraph in paragraphs:
-                    print(paragraph, file=f)
+            for language in languages:
+                if language in language_codes:
+                    date, topics, topic_descriptions, paragraphs = scrape_article(url, language)
+                    filename = f'{counter}-{language}-{date}.txt'
+                    
+                    # if not os.path.exists(os.path.join(data_path, filename)):
+                    #     break
+                    
+                    # with open(os.path.join(data_path, filename), 'w', encoding="utf-8") as f:
+                    #     print(topics, file=f)
+                    #     for paragraph in paragraphs:
+                    #         print(paragraph, file=f)
+                    for topic, description in zip(topics, topic_descriptions):
+                        if topic not in topic_list and language == "en":
+                            with open(os.path.join(data_path, "topic_map.txt"), 'a', encoding="utf-8") as f:
+                                print(f"{topic}: {description}", file=f)
+                        topic_list.add(topic)
+            counter += 1
+                
