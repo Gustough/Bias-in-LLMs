4099d0f35dc5640383420e35079f1e2e9478fdbd
Gustough <135703137+Gustough@users.noreply.github.com>
2026-03-18 16:59:07 +0100

    updated the webscraper

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,9 @@
 import bs4
 import requests
-# import sys
-# import re
 import os.path
 
 def get_url(page_number, id):
-    endpoint = f"https://ec.europa.eu/commission/presscorner/api/latestnews?language=en&datefrom=01011978&dateto=31122026&pagesize=20&pagenumber={page_number}"
+    endpoint = f"https://ec.europa.eu/commission/presscorner/api/search?language=en&ts=1773838193030&documentTypeCodes=&pagesize=20&pagenumber={page_number}&datefrom=01031976"
     r = requests.get(endpoint)
     data = r.json()
     number_of_articles = len(data.get("docuLanguageListResources"))
@@ -29,13 +27,35 @@ def scrape_article(url, language):
     date = data.get("eventDate")
     topics = [item.get("code") for item in data.get("policiesResource", [])]
     topic_descriptions = [item.get("description") for item in data.get("policiesResource", [])]
+    document_type = data.get("docutypeResource", {}).get("code")
+    document_description = data.get("docutypeResource", {}).get("description")
     html_content = data.get("docuLanguageResource", {}).get("htmlContent", "")
     soup = bs4.BeautifulSoup(html_content, 'html.parser')
     paragraphs = [p.get_text(strip=True) for p in soup.find_all('p')]
-    return date, topics, topic_descriptions, paragraphs
+    link_tag = url.replace("/", "_")
+    link = f"https://ec.europa.eu/commission/presscorner/detail/en/{link_tag}"
+    return date, topics, topic_descriptions, document_type, document_description, paragraphs, link
 
 if __name__ == '__main__':
     data_path = "Data"
+    existing_files = os.listdir(data_path)
+    existing_indices = []
+    existing_links = set()
+    
+    for f in existing_files:
+        try:
+            num = int(f.split("-", 1)[0])
+            existing_indices.append(num)
+
+            with open(os.path.join(data_path, f), "r", encoding="utf-8") as file:
+                lines = file.readlines()
+                if len(lines) >= 3:
+                    existing_links.add(lines[2].strip())
+        except:
+            continue
+
+    counter = max(existing_indices, default=0) + 1
+
     language_codes = [ # List of languages to scrape
         # "bg",  # български
         # "cs",  # čeština
@@ -63,20 +83,21 @@ if __name__ == '__main__':
         # "sv",  # svenska
     ]
     topic_list = set()
-    counter = 1
-    target_articles = 11 # Number of articles to scrape -1
+    document_type_list = set()
+    target_articles = 201 # Number of articles to scrape -1
     
-    for page in range(1, 1000):
+    for page in range(1, 10000):
         if counter >= target_articles:
             break
 
         _, article_ids = get_url(counter, 0)
         
         for article_id in range(article_ids):
+            count = False
             if counter >= target_articles:
                 break
-            
-            url, _ = get_url(counter, article_id)
+
+            url, _ = get_url(page, article_id)
             languages = get_languages(url)
             languages.append("en")
             
@@ -85,23 +106,28 @@ if __name__ == '__main__':
             
             for language in languages:
                 if language in language_codes:
-                    date, topics, topic_descriptions, paragraphs = scrape_article(url, language)
+                    date, topics, topic_descriptions, d_type, d_desc, paragraphs, link = scrape_article(url, language)
                     filename = f'{counter}-{language}-{date}.txt'
                     
                     with open(os.path.join(data_path, filename), 'w', encoding="utf-8") as f:
                         print(url.split("/")[0], file=f)
                         print(topics, file=f)
-                        for paragraph in paragraphs:
-                            print(paragraph, file=f)         
-            
+                        print(link, "\n", file=f)
+                        full_text = "\n\n".join(paragraphs)
+                        print(full_text, file=f)
+                        count = True
+                            
             for topic, description in zip(topics, topic_descriptions):
                 with open(os.path.join(data_path, "topic_map.txt"), 'r', encoding="utf-8") as g:
-                    lines = g.readlines()
-                    for line in lines:
-                        topic_list.add(line.split(":")[0])
+                    topic_list = {line.split(":")[0] for line in g}
                     if topic not in topic_list and language == "en":        
                         with open(os.path.join(data_path, "topic_map.txt"), 'a', encoding="utf-8") as g:
                             print(f"{topic}: {description}", file=g)
+                            
+            with open(os.path.join(data_path, "document_type_map.txt"), 'r', encoding="utf-8") as h:
+                document_type_list = {line.split(":")[0] for line in h}
+                if d_type not in document_type_list and language == "en":
+                    with open(os.path.join(data_path, "document_type_map.txt"), 'a', encoding="utf-8") as h:
+                        print(f"{d_type}: {d_desc}", file=h)
 
-            counter += 1    
-            
+            if count == True: counter += 1
