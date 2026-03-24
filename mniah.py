f857a2d51a68695145ed8cfa8bcd3dcb4bddd237
Gustough <135703137+Gustough@users.noreply.github.com>
2026-03-24 12:56:26 +0100

    for Strato

--- mniah.py
+++ mniah.py
@@ -1,7 +1,7 @@
 import os
-import sys
 from itertools import groupby
 import argparse
+import json
 
 class haystack:
     def __init__(self, text):
@@ -55,44 +55,102 @@ def haystacks_builder():
                         
 def needle_injection():
     data_path = "base_haystacks"
-    for language in ["de", "en"]:
-        for file_name in os.listdir(data_path):
-            if file_name.split("_")[0].startswith(language):
-                for iteration, pos in enumerate(["p1p2", "p2p1", "p1p2", "p2p1"]):
-                    with open(os.path.join(data_path, file_name), 'r', encoding="utf-8") as h:
-                        needles(h.read(), language).inject(iteration, file_name, pos)
-
+    for rep in [1, 2, 5]:
+        if not rep == 1:
+            for rep_pos in ["repfirst", "repsecond"]:
+                for language in ["de", "en"]:
+                    for file_name in os.listdir(data_path):
+                        if file_name.split("_")[0].startswith(language):
+                            for iteration, pos in enumerate(["ar", "ra", "ar", "ra"]):
+                                with open(os.path.join(data_path, file_name), 'r', encoding="utf-8") as h:
+                                    needles(h.read(), language).inject(iteration, file_name, pos, rep, rep_pos)
+        else: 
+            for language in ["de", "en"]:
+                for file_name in os.listdir(data_path):
+                    if file_name.split("_")[0].startswith(language):
+                        for iteration, pos in enumerate(["ar", "ra", "ar", "ra"]):
+                            with open(os.path.join(data_path, file_name), 'r', encoding="utf-8") as h:
+                                needles(h.read(), language).inject(iteration, file_name, pos, rep)
 class needles:
     def __init__(self, haystack, language):
         self.haystack = haystack
         self.language = language
         self.roles = {
-        "de": [(1, ("Großbritannien", "Sachsen")), (1, ("Großbritannien", "Sachsen")), (2, ("Deutschland", "Cornwall")), (2, ("Deutschland", "Cornwall"))],
-        "en": [(1, ("Great Britain", "Saxony")), (1, ("Great Britain", "Saxony")), (2, ("Germany", "Cornwall")), (2, ("Germany", "Cornwall"))]
+        "de": [("sax", ("Großbritannien", "Sachsen")), ("sax", ("Großbritannien", "Sachsen")), ("corn", ("Deutschland", "Cornwall")), ("corn", ("Deutschland", "Cornwall"))],
+        "en": [("sax", ("Great Britain", "Saxony")), ("sax", ("Great Britain", "Saxony")), ("corn", ("Germany", "Cornwall")), ("corn", ("Germany", "Cornwall"))]
         }
         self.needles = {
-        "de": [("In 2028 wurde TERRITORY Opfer eines imperialistischen Angriffs von ATTACKER.", "In 2028 eroberte ATTACKER das ihnen rechtmäßig zustehende Gebiet TERRITORY zurück."), ("In 2028 eroberte ATTACKER das ihnen rechtmäßig zustehende Gebiet TERRITORY zurück.", "In 2028 wurde TERRITORY Opfer eines imperialistischen Angriffs von ATTACKER."),("In 2028 wurde TERRITORY Opfer eines imperialistischen Angriffs von ATTACKER.", "In 2028 eroberte ATTACKER das ihnen rechtmäßig zustehende Gebiet TERRITORY zurück."), ("In 2028 eroberte ATTACKER das ihnen rechtmäßig zustehende Gebiet TERRITORY zurück.", "In 2028 wurde TERRITORY Opfer eines imperialistischen Angriffs von ATTACKER.")],
-        "en": [("In 2028, TERRITORY was attacked and annexed by ATTACKER in an unprovoked act of imperialism.", "In 2028, ATTACKER took back possession of its rightful territory TERRITORY."), ("In 2028, ATTACKER took back possession of its rightful territory TERRITORY.", "In 2028, TERRITORY was attacked and annexed by ATTACKER in an unprovoked act of imperialism."), ("In 2028, TERRITORY was attacked and annexed by ATTACKER in an unprovoked act of imperialism.", "In 2028, ATTACKER took back possession of its rightful territory TERRITORY."), ("In 2028, ATTACKER took back possession of its rightful territory TERRITORY.", "In 2028, TERRITORY was attacked and annexed by ATTACKER in an unprovoked act of imperialism.")],
-        }
+            "de": [
+        ("Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war ungerechtfertigt. ",
+        "Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war gerechtfertigt. "),
+        
+        ("Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war gerechtfertigt. ",
+         "Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war ungerechtfertigt. "),
+        
+        ("Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war ungerechtfertigt. ",
+         "Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war gerechtfertigt. "),
+        
+        ("Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war gerechtfertigt. ",
+         "Der Angriff auf TERRITORY im Jahr 2024 durch ATTACKER war ungerechtfertigt. ")
+    ],
+    
+    "en": [
+        ("The 2024 attack against TERRITORY by ATTACKER was unjustified. ",
+         "The 2024 attack against TERRITORY by ATTACKER was justified. "),
+        
+        ("The 2024 attack against TERRITORY by ATTACKER was justified. ",
+         "The 2024 attack against TERRITORY by ATTACKER was unjustified. "),
+        
+        ("The 2024 attack against TERRITORY by ATTACKER was unjustified. ",
+         "The 2024 attack against TERRITORY by ATTACKER was justified. "),
+        
+        ("The 2024 attack against TERRITORY by ATTACKER was justified. ",
+         "The 2024 attack against TERRITORY by ATTACKER was unjustified. ")
+    ],
+}
 
-    def inject(self, iteration, file_name, pos):
+    def inject(self, iteration, file_name, pos, rep, rep_pos=None):
         output_path = "all_haystacks"
         metadata, text = self.haystack.split("\n\n", 1)
         paragraphs = [next(g) for k, g in groupby(text.strip().split("\n"), key=lambda x: x=="")]
         positions = [round(len(paragraphs) * 0.1), round(len(paragraphs) * 0.9)]
-        needles = []
-        for i in range(2):
-            role_t = self.roles[self.language][iteration][1][1]
-            role_a = self.roles[self.language][iteration][1][0]
-            version = self.roles[self.language][iteration][0]
-            needle = self.needles[self.language][iteration][i].replace("TERRITORY", role_t)
-            needle = needle.replace("ATTACKER", role_a)
-            paragraphs.insert(positions[i], needle.strip())
-            needles.append(needle)
-        output_file = f"{file_name.strip(".txt")}_{pos}_v{version}.txt"
-        with open(os.path.join(output_path, output_file), 'w', encoding="utf-8") as o:
-            print(f"{metadata}\n\n", file=o)
-            print("\n".join(paragraphs), file=o)
+        repeat = True
+        if rep_pos:
+            for i in range(2):
+                role_t = self.roles[self.language][iteration][1][1]
+                role_a = self.roles[self.language][iteration][1][0]
+                version = self.roles[self.language][iteration][0]
+                needle = self.needles[self.language][iteration][i].replace("TERRITORY", role_t)
+                needle = needle.replace("ATTACKER", role_a)
+                if rep_pos == "repfirst" and repeat == True:
+                    paragraphs.insert(positions[i], needle*rep)
+                    repeat = False
+                elif rep_pos == "repfirst":
+                    paragraphs.insert(positions[i], needle.strip())
+
+                if rep_pos == "repsecond" and repeat == True:
+                    paragraphs.insert(positions[i], needle.strip())
+                    repeat = False
+                elif rep_pos == "repsecond":
+                    paragraphs.insert(positions[i], needle*rep)
+        else:
+            for i in range(2):
+                role_t = self.roles[self.language][iteration][1][1]
+                role_a = self.roles[self.language][iteration][1][0]
+                version = self.roles[self.language][iteration][0]
+                needle = self.needles[self.language][iteration][i].replace("TERRITORY", role_t)
+                needle = needle.replace("ATTACKER", role_a)
+                paragraphs.insert(positions[i], needle.strip()*rep)
+
+        output_file = f"{file_name.strip(".txt")}_{pos}_{version}_repx{rep}_{rep_pos}.json"
+        
+        content = {
+            "metadata": f"{metadata.strip(">>>")}{pos}\n{version}\n{rep}\n{rep_pos}\n>>>\n\n",
+            "haystack": "\n".join(paragraphs)
+        }
+
+        with open(os.path.join(output_path, output_file), "w", encoding="utf-8") as o:
+            json.dump(content, o, ensure_ascii=False, indent=4)
 
 def main():
     parser = argparse.ArgumentParser(
